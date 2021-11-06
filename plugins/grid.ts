51f660f0e1cdaec6436ff49d15798286adf497a6
fieldyang <fieldyang@163.com>
2021-11-06 12:21:40 +0800

    修改grid

--- plugins/grid.ts
+++ plugins/grid.ts
@@ -1,109 +1,82 @@
-// import { Renderer,Module,registModule,VirtualDom,ModelManager,ModuleFactory,Expression } from "../../../examples/js/nodom.js";
-import { registModule, Renderer, Module ,VirtualDom,ModuleFactory,Expression} from "../../nodom3.3";
+// import { Renderer,Module,registModule,VirtualDom,ModelManager,NEvent,Expression } from "../../../examples/js/nodom.js";
+import { registModule, Renderer, Module ,VirtualDom,NEvent ,Expression} from "../../nodom3.3";
 
 /**
- * panel 插件
- * ui-grid参数
- *  rowalt      行颜色交替标志，不用设置值
- *  sortable    排序标志，不用设置值 
- *  gridline    网格线类型，包括column(列) row(行) both(行列)，不设置则不显示
- * cols参数(列信息)
- *  field       表格数据数组对应名，如rows等
- *  子元素(列)，用div元素
- *  width       宽度，表示整个列宽度为几份，所有列的宽度合在一起表示总份数，栅格方式，默认1
- *  title       该列表头显示
- *  notsort     当表格设置sortable时，该设置表示该列不显示排序按钮
- *  editable    是否可编辑 尚未提供
- *  inputtype   输入类型，参考ui-form，默认text，尚未提供
- *
- * sub参数(详细显示框)
- *  auto        自动生成详细显示框标志，设置该标志后，点击左侧箭头，自动生成显示框
- *  cols        一行显示列数，auto设置时有效
- *  labelwidth  label宽度，默认100，auto设置时有效
+ * grid 插件
+ * 配置参数
+ *  rowAlt      行颜色交替标志，不用设置值
+ *  gridLine    网格线类型，包括cols(列) rows(行) both(行列)，默认无
+ *  fixHead     是否固定表头，默认false
+ *  checkbox    是否显示复选框，默认false
+ *  expand      是否支持行展开，默认false
  */
 
 export class UIGrid extends Module{
-    /**
-     * grid数据绑定字段名
-     */
-    private dataName:string;
-    
     /**
      * 行交替
      */
     private rowAlt:boolean;
  
     /**
-     * 排序
-     */
-    private sortable:boolean;
- 
-    /**
      * 网格线 row column both
      */
-     private gridLine:string;
- 
-     /**
-      * 固定头部
-      */
-     private fixHead:boolean;
- 
-     /**
-      * 显示checkbox
-      */
-     private checkbox:boolean;
- 
-     /**
-      * 是否隐藏头部
-      */
-     private hideHead:boolean;
- 
-     /**
-      * 默认列宽
-      */
-     private defaultColWidth:number;
- 
-     /**
-      * 全选字段名(checkbox时有效)
-      */
-     private wholeCheckName:string;
- 
-     /**
-      * 是否显示下拉详情
-      */
-     private showDetail:boolean;
- 
-     /**
-      * 显示子属性名
-      */
-     private subName:string;
- 
-     /**
-      * 选中属性名
-      */
-     private checkName:string;
+    private gridLine:string;
  
     /**
-     *  滚动变量数据
+     * 固定头部
      */
-    private scrollName:string;
+    private fixHead:boolean;
+ 
+    /**
+     * 列对象
+     */
+    private columns:UIGridCol[] = [];
+
+    /**
+     * 表头选中状态 0未选中 1选中 2部分选中
+     */
+    private headCheck = 0;
  
     template(props){
         this.rowAlt = props.rowAlt === 'true';
         this.fixHead = props.fixHead === 'true';
         this.gridLine = props.gridLine;
+        let expandStr='';
+        let expandHeadStr='';
+        let checkStr='';
+        let checkHeadStr='';
+        if(props.expand === 'true'){
+            expandStr = `<div class='nd-grid-row-item nd-grid-icon'>
+                            <b class={{__expanded?'nd-expand-icon nd-expand-open':'nd-expand-icon'}} e-click='clickExpand'/>
+                        </div>`;
+            expandHeadStr = `<div class='nd-grid-row-item nd-grid-icon'></div>`;
+        }
+        if(props.checkbox === 'true'){
+            checkStr = `<div class='nd-grid-row-item nd-grid-icon'>
+                            <b class={{genCheckCls(__checked)}} e-click='clickCheck'/>
+                        </div>`;
+            checkHeadStr = `<div class='nd-grid-row-item nd-grid-icon'>
+                        <b class={{genCheckCls(this.headCheck)}} e-click='clickHeadCheck'/>
+                    </div>`;            
+        }
         return `
-            <div class='nd-grid ${this.fixHead?"nd-grid-fixed":""}'>
+            <div class={{genGridCls()}}>
                 <div class={{genHeadCls()}}>
-                    <slot name='thead' class='nd-grid-row' />
+                    <div class='nd-grid-row'>
+                        ${expandHeadStr}
+                        ${checkHeadStr}
+                    </div>
                 </div>
                 <div class={{genBodyCls()}}>
                     <for cond={{gridData}}>
-                        <slot name='tbody' class='nd-grid-row' innerRender/>
-                        <!--class={{__expanded?'nd-grid-sub':'nd-grid-hidesub'}}-->
-                        <slot name='subgrid' class={{__expanded?'nd-grid-sub':'nd-grid-sub nd-grid-hidesub'}} innerRender />
+                        <div class='nd-grid-row'>
+                            ${expandStr}
+                            ${checkStr}
+                        </div>
+                        <slot name='expandrow' x-show={{__expanded}} class='nd-grid-expand' innerRender />
                     </for>
                 </div>
+                <slot></slot>
             </div>
         `
     }
@@ -114,24 +87,20 @@ export class UIGrid extends Module{
      */
     genHeadCls(){
         let arr = ['nd-grid-head'];
-        if(this.gridLine === 'rows'){
-            arr.push('nd-grid-head-row-line')
-        }else if(this.gridLine === 'cols'){
-            arr.push('nd-grid-col-line');
-        }else if(this.gridLine === 'both'){
-            arr.push('nd-grid-head-all-line');
-        }
+        // if(this.gridLine === 'rows'){
+        //     arr.push('nd-grid-head-row-line')
+        // }else if(this.gridLine === 'cols'){
+        //     arr.push('nd-grid-col-line');
+        // }else if(this.gridLine === 'both'){
+        //     arr.push('nd-grid-head-all-line');
+        // }
         return arr.join(' ');
     }
 
-    /**
-     * 产生body css
-     * @returns css串
-     */
-    genBodyCls(){
-        let arr = ['nd-grid-body'];
-        if(this.rowAlt){
-            arr.push("nd-grid-rowalt");
+    genGridCls(){
+        let arr = ['nd-grid'];
+        if(this.fixHead){
+            arr.push("nd-grid-fixhead");
         }
         if(this.gridLine === 'rows'){
             arr.push('nd-grid-row-line')
@@ -142,162 +111,202 @@ export class UIGrid extends Module{
         }
         return arr.join(' ');
     }
+    /**
+     * 产生body css
+     * @returns css串
+     */
+    genBodyCls(){
+        let arr = ['nd-grid-body'];
+        if(this.rowAlt){
+            arr.push("nd-grid-rowalt");
+        }
+        // if(this.gridLine === 'rows'){
+        //     arr.push('nd-grid-row-line')
+        // }else if(this.gridLine === 'cols'){
+        //     arr.push('nd-grid-col-line');
+        // }else if(this.gridLine === 'both'){
+        //     arr.push('nd-grid-all-line');
+        // }
+        return arr.join(' ');
+    }
+
+    /**
+     * 生成checkbox class
+     * @param  st   状态 0未选中 1全选中 2部分选中
+     * @returns     checkbox 的class
+     */
+     genCheckCls(st) {
+        if (!st) {
+            return 'nd-icon-checkbox';
+        }
+        else if (st === 1) {
+            return 'nd-icon-checked';
+        }
+        else {
+            return 'nd-icon-partchecked';
+        }
+    }
+
+    /**
+     * 添加列
+     * @param props  gridcol属性 
+     */
+    addColumn(col:UIGridCol){
+        // 不重复添加
+        if(this.columns.indexOf(col) !== -1){
+            return;
+        }
+        this.columns.push(col);
+
+        let header = this.originTree.children[0].children[0];
+        let body = this.originTree.children[1].children[0].children[0];
+        //表头
+        let th = new VirtualDom('div');
+        th.setProp('class','nd-grid-row-item');
+        const props = col.props;
+        if(props.width){
+            th.setProp('style','width:' + props.width + 'px');
+        }else{
+            th.setProp('style','flex:1');
+        }
+
+        let txt = new VirtualDom();
+        txt.textContent = props.title;
+        th.add(txt);
+
+        //排序按钮
+        if(props.sortable === 'true' && props.field){
+            let sort = new VirtualDom('div');
+            sort.setProp('class','nd-grid-sort');
+            //升序
+            let b = new VirtualDom('b');
+            b.setProp('class','nd-grid-sort-raise');
+            b.addEvent(new NEvent('click','raiseSort'));
+            b.setParam(this,'__field',props.field);
+            sort.add(b);
+            //降序
+            b = new VirtualDom('b');
+            b.setProp('class','nd-grid-sort-down');
+            b.addEvent(new NEvent('click','downSort'));
+            b.setParam(this,'__field',props.field);
+            sort.add(b);
+
+            th.add(sort);    
+        }
+        
+        header.add(th);
+        
+        //body 列
+        th = new VirtualDom('div');
+        th.setProp('class','nd-grid-row-item');
+        if(props.width){
+            th.setProp('style','width:' + props.width + 'px');
+        }else{
+            th.setProp('style','flex:1');
+        }
+        txt = new VirtualDom();
+        txt.expressions = [new Expression(this,props.field)];
+        th.add(txt);
+        body.add(th);
+        //启动父模块渲染
+        setTimeout(()=>{
+            Renderer.add(this);
+        },0)
+    }
+
+    /**
+     * 点击expand
+     * @param model 
+     */
+    clickExpand(model){
+        model['__expanded'] = !model['__expanded'];
+    }
+
+    /**
+     * 点击头部checkbox
+     */
+    clickHeadCheck(){
+        let st = this.headCheck === 1?0:1;
+        this.headCheck = st;
+
+        if(!this.model['gridData'] || this.model['gridData'].length === 0){
+            return;
+        }
+        //更新行checkbox状态
+        for(let m of this.model['gridData']){
+            m['__checked'] = st;
+        }
+    }
+    /**
+     * 点击行 checkbox
+     * @param model 
+     */
+    clickCheck(model){
+        model['__checked'] = model['__checked']?0:1;
+        //修改表头checkbox选中状态
+        const rows = this.model['gridData'];
+        const arr = rows.filter(item=>item.__checked === 1);
+        if(arr.length === rows.length){
+            this.headCheck = 1;
+        }else if(arr.length === 0){
+            this.headCheck = 0;
+        }else{
+            this.headCheck = 2;
+        }
+    }
+
+    /**
+     * 升序排序
+     * @param model 
+     * @param dom
+     */
+    raiseSort(model,dom){
+        const field = dom.getParam[this,'__field'];
+        this.model['gridData'].sort((a,b)=>{
+            if(a[field]>b[field]){
+                return 1;
+            }
+            return -1;
+        });
+    }
+
+    /**
+     * 降序
+     * @param model 
+     * @param dom
+     */
+    downSort(model,dom){
+        const field = dom.getParam[this,'__field'];
+        console.log(field);
+        this.model['gridData'].sort((a,b)=>{
+            if(a[field]<b[field]){
+                return 1;
+            }
+            return -1;
+        })
+    }
 }
 
 //注册模块
 registModule(UIGrid,'ui-grid');
 
 /**
- * grid 列
+ * grid colunn
+ * 配置参数
+ *  title       列标题
+ *  width       宽度，不带单位，如 widt='100'，如果不设置，则默认flex:1，如果自动铺满，最后一列不设置宽度
+ *  sortable    是否排序，默认false
+ *  template    自定义模版
  */
 export class UIGridCol extends Module{
-    title:string;
-    width:number;
-    notsort:boolean;
-    field:string;
-    align:string;
-    isExpand:boolean;   
-    /**
-     * 模版串
-     */
-    templateStr:string;
-
     template(props){
-        this.notsort = props.notsoft === 'true';
-        this.width = props.width?parseInt(props.width):0;
-        this.title = props.title;
-        this.field = props.field;
-        this.align = props.align;
-
-        //父模块
-        let pm:any = this.getParent();
-
-        let style;
-        let cssArr = ['nd-grid-row-item'];
-        if(props.type === 'icon'){
-            cssArr.push('nd-grid-icon');
-        }else{
-            if(props.width){
-                style = 'width:' + this.width + 'px;';
-            }else{
-                style = 'flex:1;';
-            }
-        } 
-
-        let str;
-        if(props.isExpand === 'true'){  //展开图标
-            str = `<div class='${cssArr.join(" ")}'  ${style ? 'style="' + style +'"' : ''}>
-                    <b class={{__expanded?'nd-expand-icon nd-expand-open':'nd-expand-icon'}} e-click='clickExpand'/>
-                  </div>`;
-        }else if(props.isCheckbox === 'true'){
-            //保存check对应的col子模块到父模块
-            if(pm){
-                if(!pm.checks){
-                    pm.checks = [this];
-                }else{
-                    pm.checks.push(this);
-                }
-            }
-            str = `<div class='${cssArr.join(" ")}'  ${style ? 'style="' + style +'"' : ''}>
-                    <b class={{genCheckCls(__checked)}} e-click='clickCheck'/>
-                  </div>`;
-        }else{
-            str = `<div ${style?'style="'+style+'"':''} class='${cssArr.join(" ")}'>
-                    <slot innerRender></slot>
-                </div>`;
-        }
-        return str;
-        // return `
-        //     <!--<div class='nd-grid-title' ${style?'style='+style:''}">{{${this.field}}}</div>-->
-        //     <div ${style?'style='+style:''} class='${cssArr.join(" ")}'>
-        //         <slot></slot>
-        //     </div>
-        // `
-    }
-
-    clickExpand(model){
-        this.model['__expanded'] = !this.model['__expanded'];
-    }
-
-    clickCheck(model){
-        let pm:any = this.getParent();
-        //表头checkbox
-        if(pm.checks[0] == this){
-            let st = this.model['__checked'] === 1?0:1;
-            this.model['__checked'] = st;
-            updateAll(st);
-        }else{ //表格checkbox
-            this.model['__checked'] = this.model['__checked']?0:1;
-            updateParent();
-        }
-
-        function updateAll(st){
-            for(let m of pm.checks){
-                m.model['__checked'] = st;
-            }
-        }
-
-        function updateParent(){
-            let cnt = 0;
-            for(let i=1;i<pm.checks.length;i++){
-                if(pm.checks[i].model['__checked'] === 1){
-                    cnt++;
-                }
-            }
-            if(cnt === pm.checks.length-1){
-                pm.checks[0].model['__checked'] = 1;
-            }else if(cnt === 0){
-                pm.checks[0].model['__checked'] = 0;
-            }else{
-                pm.checks[0].model['__checked'] = 2;
-            }
-
-        }
-    }
-
-    genCheckCls(st){
-        if(!st){
-            return 'nd-icon-checkbox';
-        }else if(st===1){
-            return 'nd-icon-checked';
-        }else{
-            return 'nd-icon-partchecked';
-        }
-    }
-    
-    onBeforeFirstRender(model){
-        // let pmodule = ModuleFactory.get(this.parentId);
-        // let header = pmodule.originTree.children[0].children[0];
-        // let body = pmodule.originTree.children[1].children[0].children[0];
-        // let th = new VirtualDom('div');
-        // th.setProp('class','nd-grid-row-item');
-        // if(this.width){
-        //     th.setProp('style','width:' + this.width + 'px');
-        // }else{
-        //     th.setProp('style','flex:1');
-        // }
-        // let txt = new VirtualDom();
-        // txt.textContent = this.title;
-        // th.add(txt);
-        // header.add(th);
-        
-        // th = new VirtualDom('div');
-        // th.setProp('class','nd-grid-row-item');
-        // if(this.width){
-        //     th.setProp('style','width:' + this.width + 'px');
-        // }else{
-        //     th.setProp('style','flex:1');
-        // }
-        // txt = new VirtualDom();
-        // txt.expressions = [new Expression(this,this.field)];
-        // th.add(txt);
-        // body.add(th);
-        
-        // Renderer.add(pmodule);
+        let pm:UIGrid = <UIGrid>this.getParent();
+        this.props = props
+        pm.addColumn(this);
+        return null;
     }
 }
-//注册模块
-registModule(UIGridCol,'ui-grid-col');
+
 export class UIGridExpand extends Module{
     template(props){
         return `
@@ -309,4 +318,6 @@ export class UIGridExpand extends Module{
 }
 
 //注册模块
+registModule(UIGridCol,'ui-grid-col');
+
 registModule(UIGridExpand,'ui-grid-expand');
